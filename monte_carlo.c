417547c7bffdaeaad24ca96d25e23ed40c2e4639
S4srihari <85178734+S4srihari@users.noreply.github.com>
2021-06-03 22:54:28 +0530

    Create monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -7,15 +7,18 @@ float mc_pi(int);
 
 float mc_pi(int n)
 { int incircle =0;
- for(int i=0;i<n;i++){
+ for(int i=0;i<n;i++)
+ {
    float x=frandom();
    float y=frandom();
    float d=x*x + y*y;
-   if(d<=1){incircle++
-  }
- }
+   if(d<=1)
+   {incircle++
+    }
+   }
    float pi =4*(float)incircle/n;
-   return pi;}
+   return pi;
+}
 
 float frandom() {
   long int q = random();
